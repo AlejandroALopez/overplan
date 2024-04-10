9bbca82f659bc0cea95b79437928b55eac01a74d
alejandro <lopez.alejandro@dali.dartmouth.edu>
2024-04-10 15:42:56 -0400

    landing page grayscale

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,40 +7,41 @@ export default function Home() {
   const app_name = "OverGoal AI";
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col">
       <Header />
-      <div className="Landing">
-        <div className="presentation-section-container">
-          <div className="presentation-info-container">
-            <p>
+      <div className="p-16 bg-[#f5f5f5]">
+        <div className="flex flex-row justify-between">
+          <div className="flex flex-col w-5/12">
+            <p className="text-3xl">
               Want to make a detailed plan to reach your goal?
-              Make one in seconds with <span>{app_name}</span>!
+              Make one in seconds with <span className="font-bold">{app_name}</span>!
             </p>
-            <p><span>{app_name}</span> uses AI to build the perfect plan for you!</p>
-            <div className="button-container">
+            <p className="mt-12 text-3xl"><span className="font-bold">{app_name}</span> uses AI to build the perfect plan for you!</p>
+            <div className="mt-8 text-right">
               <Link href="/plan/goal">
-                <button className="start-button">Get Started</button>
+                <button className="w-36 p-4 border-none rounded-3xl bg-gray-500 text-white 
+                   cursor-pointer drop-shadow-lg transition hover:scale-110 duration-300">Get Started</button>
               </Link>
             </div>
           </div>
-          <div className="presentation-image" />
+          <div className="w-96 h-80 bg-[#d9d9d9]" />
         </div>
-        <div className="features-section-container">
-          <div className="features-title-container">
-            <p>What {app_name} offers</p>
+        <div className="flex flex-col items-center mt-16">
+          <div className="flex mb-4">
+            <p className="text-2xl font-bold">What {app_name} offers</p>
           </div>
-          <div className="features-container">
-            <div className="feature-item">
-              <div className="feature-image" />
-              <p className="feature-text">Build a plan fast by answering a few questions</p>
+          <div className="flex flex-row justify-evenly">
+            <div className="flex flex-col items-center w-2/6">
+              <div className="w-72 h-64 my-6 bg-[#d9d9d9]" />
+              <p className="w-4/5 text-center text-xl">Build a plan fast by answering a few questions</p>
             </div>
-            <div className="feature-item">
-              <div className="feature-image" />
-              <p className="feature-text">Get a plan organized in weekly sprints that keeps track of completed steps</p>
+            <div className="flex flex-col items-center w-2/6">
+              <div className="w-72 h-64 my-6 bg-[#d9d9d9]" />
+              <p className="w-4/5 text-center text-xl">Get a plan organized in weekly sprints that keeps track of completed steps</p>
             </div>
-            <div className="feature-item">
-              <div className="feature-image" />
-              <p className="feature-text">Edit any part of the plan at any time to fit your schedule</p>
+            <div className="flex flex-col items-center w-2/6">
+              <div className="w-72 h-64 my-6 bg-[#d9d9d9]" />
+              <p className="w-4/5 text-center text-xl">Edit any part of the plan at any time to fit your schedule</p>
             </div>
           </div>
         </div>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,15 +2,15 @@ import Link from "next/link";
 
 export default function Header() {
     return (
-        <div className="Header">
+        <div className="flex flex-row w-full items-center text-center gap-16 py-4 px-8 bg-red-700">
             <Link href="/">
-                <button className="header-button">Home</button>
+                <button className="text-white text-xl font-bold transition hover:scale-110 duration-300">Home</button>
             </Link>
             <Link href="/plan/goal">
-                <button className="header-button">Create a Plan</button>
+                <button className="text-white text-xl font-bold transition hover:scale-110 duration-300">Create a Plan</button>
             </Link>
             <Link href="/week">
-                <button className="header-button">My Week</button>
+                <button className="text-white text-xl font-bold transition hover:scale-110 duration-300">My Week</button>
             </Link>
         </div>
     );
