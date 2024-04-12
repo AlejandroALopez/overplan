254902c8863b674c7a366c03c0731e1c9d2a3556
alejandro <lopez.alejandro@dali.dartmouth.edu>
2024-04-12 16:28:04 -0400

    back button between planner pages

--- src/app/planner/weeks/page.tsx
+++ src/app/planner/weeks/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import Link from "next/link";
+import Image from 'next/image';
 import { useState } from "react";
 import { useAppSelector, useAppDispatch } from "@/lib/store";
 import { WeekOptionProps } from "@/lib/types/plannerProps";
 import { setNumWeeks } from "@/lib/store/inputSlice";
+import back from "../../../../public/arrows/back.svg";
 
 const WeekSelector: React.FC<WeekOptionProps> = ({ weeks, setWeeks }) => {
     const title = "In how many weeks do you wish to complete it?";
@@ -15,18 +17,18 @@ const WeekSelector: React.FC<WeekOptionProps> = ({ weeks, setWeeks }) => {
         <div className="flex flex-col my-12">
             <p className="text-2xl font-bold">{title}</p>
             <div className="flex flex-row items-center gap-4 my-8">
-                <button 
+                <button
                     className={`px-6 py-4 rounded-2xl drop-shadow-md 
                     transition hover:scale-110 duration-300 cursor-pointer
-                    ${weeks >= MIN_VALUE ? "bg-gray-400" : "bg-gray-300"}`}
-                    onClick={() => (weeks >= MIN_VALUE) ? setWeeks(weeks - 1) : null}
+                    ${weeks >= MIN_VALUE + 1 ? "bg-gray-400" : "bg-gray-300"}`}
+                    onClick={() => (weeks >= MIN_VALUE + 1) ? setWeeks(weeks - 1) : null}
                 >
                     <p className="text-3xl">-</p>
                 </button>
                 <div className="flex items-center justify-center w-32 h-16 rounded-2xl border-2 border-solid border-gray-400">
                     <p className="text-3xl">{weeks}</p>
                 </div>
-                <button 
+                <button
                     className={`bg-gray-400 px-6 py-4 rounded-2xl drop-shadow-md 
                     transition hover:scale-110 duration-300 cursor-pointer
                     ${weeks <= MAX_VALUE - 1 ? "bg-gray-400" : "bg-gray-300"}`}
@@ -43,16 +45,19 @@ export default function SetWeeks() {
     const dispatch = useAppDispatch();
     const goal = useAppSelector(state => state.input.goal);
 
-    const [weeks, setWeeks] = useState<number>(0);
+    const [weeks, setWeeks] = useState<number>(4);
 
     const submitTime = () => {
-        console.log("# weeks: ", weeks);
         dispatch(setNumWeeks(weeks));
         // TODO: API call to create plan
     }
 
     return (
         <main className="flex min-h-screen flex-col p-8 bg-[#f5f5f5]">
+            <Link href="/planner/goal" className="flex flex-row items-center gap-2 w-20 mb-8 transition hover:scale-110 duration-300">
+                <Image src={back} alt="back button" />
+                <p>Back</p>
+            </Link>
             <p className="text-gray-500 text-xl">
                 Goal: <span className="text-[#a52a2a] font-bold">{goal}</span>
             </p>
