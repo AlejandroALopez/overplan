9fb737bb20788d2c9fa7cae3b1859af45f89c723
alejandro <lopez.alejandro@dali.dartmouth.edu>
2024-05-30 11:34:34 -0400

    progress on subscriptions screen

--- src/app/dashboard/subscriptions/page.tsx
+++ src/app/dashboard/subscriptions/page.tsx
@@ -6,7 +6,7 @@ import { useState } from 'react';
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY || "");
 
 export default function SubscriptionPage() {
-    const [email, setEmail] = useState('');
+    const [annualOption, setAnnualOption] = useState<boolean>(true);
 
     const handleSubscribe = async (priceId: string) => {
         const stripe = await stripePromise;
@@ -27,7 +27,7 @@ export default function SubscriptionPage() {
 
         const { error } = await stripe.redirectToCheckout({
             sessionId,
-          });
+        });
 
         if (error) {
             console.error(error.message);
@@ -35,28 +35,69 @@ export default function SubscriptionPage() {
     };
 
     return (
-        <div>
-            <h1>Choose a Plan</h1>
-            <input
-                type="email"
-                placeholder="Enter your email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-            />
-            <div>
-                <h2>Free Plan</h2>
-                <p>$0/mo</p>
-                <button onClick={() => handleSubscribe(process.env.NEXT_PUBLIC_STRIPE_FREE_PRICE_ID || "")}>
-                    Subscribe
+        <div className='flex flex-col items-center w-full gap-8 p-12 bg-white'>
+            <div className='flex flex-col items-center gap-8'>
+                <p className="text-4xl font-semibold text-center w-1/2">Achieve More With OverPlan AI Premium Plans</p>
+                <p className='text-lg text-[#808080] text-center w-3/4'>
+                    Get the most out of OverPlan AI plan generator and our plan management tools.
+                    Progress through multiple plans, get more personalized tasks, and more with
+                    OverPlan premium subscriptions.
+                </p>
+            </div>
+            {/* <h1>Choose a Plan</h1> */}
+            <div className='flex flex-row gap-4'>
+                <button
+                    className={`py-2 px-2 border-none bg-primary 
+                        ${annualOption ? "bg-opacity-15" : "text-white"} rounded-md
+                        transition hover:scale-105 duration-300`}
+                    onClick={() => setAnnualOption(false)}
+                >
+                    Monthly
+                </button>
+                <button
+                    className={`py-2 px-2 border-none bg-primary 
+                        ${!annualOption ? "bg-opacity-15" : "text-white"} rounded-md
+                        transition hover:scale-105 duration-300`}
+                    onClick={() => setAnnualOption(true)}
+                >
+                    Annual (20% off)
                 </button>
             </div>
-            <div>
-                <h2>Pro Plan</h2>
-                <p>$5/mo</p>
-                <button onClick={() => handleSubscribe(process.env.NEXT_PUBLIC_STRIPE_PRO_PRICE_ID || "")}>
-                    Subscribe
-                </button>
+            <div className='flex flex-row gap-16 flex-wrap'>
+                {/* Free Plan */}
+                <div className='flex flex-col items-center px-12 py-4 gap-8 bg-[#CCCCCC] bg-opacity-10 rounded-md'>
+                    <p className='text-xl'>Free</p>
+                    <p className='text-3xl font-semibold'>$0/mo</p>
+                    <div className='flex flex-col'>
+                        <p>1 Free Plan</p>
+                    </div>
+                    <button
+                        className='bg-[#B3B3B3] text-white rounded-md px-12 py-2 mt-12 drop-shadow-md'
+                        onClick={() => handleSubscribe(process.env.NEXT_PUBLIC_STRIPE_FREE_PRICE_ID || "")}
+                        disabled
+                    >
+                        Your Plan
+                    </button>
+                </div>
+                {/* Pro Plan */}
+                <div className='flex flex-col items-center px-12 py-4 gap-8 bg-primary bg-opacity-10 rounded-md'>
+                    <p className='text-xl'>Pro</p>
+                    <div className='flex flex-col items-center gap-1'>
+                        <p className='text-3xl font-semibold'>${annualOption ? "4" : "5"}/mo</p>
+                        <p className='text-sm text-[#808080]'>{annualOption ? "billed $48 annually" : "billed monthly"}</p>
+                    </div>
+                    <div className='flex flex-col'>
+                        <p>1 Free Plan</p>
+                    </div>
+                    <button
+                        className='bg-primary text-white rounded-md px-12 py-2 mt-12 drop-shadow-md
+                            transition hover:scale-105 duration-300'
+                        onClick={() => handleSubscribe(process.env.NEXT_PUBLIC_STRIPE_PRO_PRICE_ID || "")}
+                    >
+                        Upgrade
+                    </button>
+                </div>
             </div>
-        </div>
+        </div >
     );
 }
