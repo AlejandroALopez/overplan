080e1c6160364ac74353de35f94bbddeb8fb1499
alejandro <lopez.alejandro@dali.dartmouth.edu>
2024-06-27 11:31:51 -0400

    navigation component, account and subs tabs

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -8,8 +8,8 @@ import Home from "../../public/icons/home.svg";
 import Map from "../../public/icons/map.svg";
 import Badge from "../../public/icons/award.svg";
 import Menu from "../../public/icons/menu.svg";
-import Settings from "../../public/icons/settings.svg";
-import Logout from "../../public/icons/logout.svg";
+import User from "../../public/icons/user.svg";
+import CreditCard from "../../public/icons/creditCard.svg";
 
 
 export default function Navigation() {
@@ -29,6 +29,7 @@ export default function Navigation() {
 
   return (
     <>
+      {/* Vertical Menu for Big Screens */}
       <div className="flex flex-row sm:flex-col gap-8 m-0 sm:w-52 shrink-0 px-4 py-2 sm:px-0 sm:py-8 bg-white text-center min-h-full">
         <div className="flex text-center justify-between sm:justify-center items-center w-full">
           <p className="text-2xl font-semibold">{app_name}</p>
@@ -36,36 +37,39 @@ export default function Navigation() {
             <Image src={Menu} alt="Menu" />
           </button>
         </div>
-        <div className="hidden sm:flex flex-row sm:flex-col items-center gap-4 mt-2">
-          <Link href="/dashboard/week" className={`flex flex-row items-center justify-center gap-4 w-11/12 py-3 bg-opacity-10 rounded-md
+        <div className="hidden sm:flex flex-row sm:flex-col items-center justify-between gap-4 mt-2 h-full">
+          {/* Top Buttons */}
+          <div className="flex flex-col items-center w-full">
+            <Link href="/dashboard/week" className={`flex flex-row items-center gap-4 w-11/12 py-3 px-6 bg-opacity-10 rounded-md
           ${pathname === '/dashboard/week' ? "bg-primary" : "bg-white"} transition hover:scale-105 duration-300`}>
-            <Image src={Home} alt="my weeks icon" />
-            <p className="font-medium">My Week</p>
-          </Link>
-          <Link href="/dashboard/plans" className={`flex flex-row items-center justify-center gap-4 w-11/12 py-3 bg-opacity-10 rounded-md
+              <Image src={Home} alt="my weeks icon" />
+              <p className="font-medium">My Week</p>
+            </Link>
+            <Link href="/dashboard/plans" className={`flex flex-row items-center gap-4 w-11/12 py-3 px-6 bg-opacity-10 rounded-md
           ${pathname === '/dashboard/plans' ? "bg-primary" : "bg-white"} transition hover:scale-105 duration-300`}>
-            <Image src={Map} alt="my plans icon" />
-            <p className="font-medium">My Plans</p>
-          </Link>
-          {/* <Link href="/dashboard/badges" className={`flex flex-row items-center justify-center gap-4 w-11/12 py-3 bg-opacity-10 rounded-md
+              <Image src={Map} alt="my plans icon" />
+              <p className="font-medium">My Plans</p>
+            </Link>
+            {/* <Link href="/dashboard/badges" className={`flex flex-row items-center gap-4 w-11/12 py-3 px-6 bg-opacity-10 rounded-md
           ${pathname === '/dashboard/badges' ? "bg-primary" : "bg-white"} transition hover:scale-105 duration-300`}>
             <Image src={Badge} alt="my badges icon" />
             <p className="font-medium">My Badges</p>
           </Link> */}
-          <div className="bg-[#D9D9D9] h-0.5 w-11/12  rounded-md" />
-          <Link href="/dashboard/settings" className={`flex flex-row items-center justify-center gap-4 w-11/12 py-3 bg-opacity-10 rounded-md
+          </div>
+          {/* Bottom Buttons */}
+          <div className="flex flex-col items-center w-full">
+            <div className="bg-[#D9D9D9] h-0.5 w-11/12 mb-4  rounded-md" />
+            <Link href="/dashboard/account" className={`flex flex-row items-center gap-4 w-11/12 py-3 px-6 bg-opacity-10 rounded-md
           ${pathname === '/dashboard/settings' ? "bg-primary" : "bg-white"} transition hover:scale-105 duration-300`}>
-            <Image src={Settings} alt="settings icon" />
-            <p className="font-medium">Settings</p>
-          </Link>
-          <button
-            onClick={handleLogout}
-            className='flex flex-row items-center justify-center gap-4 w-11/12 py-3 bg-opacity-10 rounded-md
-               transition hover:scale-105 duration-300'>
-            <Image src={Logout} alt="logout icon" />
-            <p className="font-medium">Log Out</p>
-          </button>
-          <div className="bg-[#D9D9D9] h-0.5 w-11/12 rounded-md" />
+              <Image src={User} alt="account icon" />
+              <p className="font-medium">Account</p>
+            </Link>
+            <Link href="/dashboard/subscriptions" className={`flex flex-row items-center gap-4 w-11/12 py-3 px-6 bg-opacity-10 rounded-md
+          ${pathname === '/dashboard/subscriptions' ? "bg-primary" : "bg-white"} transition hover:scale-105 duration-300`}>
+              <Image src={CreditCard} alt="subscriptions icon" />
+              <p className="font-medium">Subscriptions</p>
+            </Link>
+          </div>
         </div>
       </div>
       {/* Accordion Menu for Smaller Screens */}
