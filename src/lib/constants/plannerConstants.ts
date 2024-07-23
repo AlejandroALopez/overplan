d1b9255a8275c52b58652fe9d575168e881f1a30
alejandro <lopez.alejandro@dali.dartmouth.edu>
2024-07-23 17:13:14 -0400

    max number of weeks increased to 24 (six months)

--- src/lib/constants/plannerConstants.ts
+++ src/lib/constants/plannerConstants.ts
@@ -7,7 +7,7 @@ export const GOAL_PLACEHOLDER: string = "Run 5 miles/day, eat healthier, etc.";
 export const WEEK_QUESTION: string = "In how many weeks do you want to achieve this goal?";
 export const DEFAULT_WEEKS: number = 4;
 export const MIN_WEEKS: number = 1;
-export const MAX_WEEKS: number = 20;
+export const MAX_WEEKS: number = 24; // 6 months
 
 // Dates Page
 export const DATES_QUESTION: string = "When do you want to start?";
