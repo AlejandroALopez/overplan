55739681c89a4b22afc6c70059102609ae4a7be9
alejandro <lopez.alejandro@dali.dartmouth.edu>
2024-08-13 12:12:57 -0400

    removed yearly price

--- src/app/dashboard/subscriptions/page.tsx
+++ src/app/dashboard/subscriptions/page.tsx
@@ -3,13 +3,11 @@
 import { getTokensFromCookies } from '@/lib/utils/auth';
 import { useAppSelector } from "@/lib/store";
 import { loadStripe } from '@stripe/stripe-js';
-import { useState } from 'react';
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY || "");
 
 export default function SubscriptionPage() {
     const userData = useAppSelector(state => state.session.userData);
-    const [annualOption, setAnnualOption] = useState<boolean>(true);
 
     const handleSubscribe = async (priceId: string) => {
         if (!userData) {
@@ -52,26 +50,6 @@ export default function SubscriptionPage() {
                     Progress through multiple plans, get more personalized tasks, and more!
                 </p>
             </div>
-            {/* Buttons */}
-            <div className='flex flex-row items-center justify-center gap-4 my-4'>
-                <button
-                    className={`flex items-center justify-center rounded-2xl px-4 py-2 drop-shadow-lg
-                    transition hover:scale-110 duration-300 ${!annualOption ? "bg-primary" : "bg-white"}`}
-                    onClick={() => setAnnualOption(false)}
-                >
-                    <p className={`md:text-lg font-medium ${!annualOption ? "text-white" : "text-black"}`}>Monthly</p>
-                </button>
-                <button
-                    className={`flex items-center justify-center rounded-2xl px-4 py-2 drop-shadow-lg
-                    transition hover:scale-110 duration-300 ${annualOption ? "bg-primary" : "bg-white"}`}
-                    onClick={() => setAnnualOption(true)}
-                >
-                    <p className={`md:text-lg font-medium ${annualOption ? "text-white" : "text-black"}`}>Yearly</p>
-                </button>
-                <div className="flex items-center justify-center h-6 px-2 bg-primary bg-opacity-20 ">
-                    <p className="text-primary font-medium">20% off</p>
-                </div>
-            </div>
             {/* Plans */}
             <div className='flex flex-col-reverse lg:flex-row justify-center gap-16 mb-12"'>
                 {/* Free Plan */}
@@ -94,56 +72,27 @@ export default function SubscriptionPage() {
                         </button>
                     )}
                 </div>
-                {/* Pro Plan (2 options) */}
-                {annualOption ? (
-                    // Pro Plan (Yearly price)
-                    <div className="flex flex-col items-center bg-white rounded-2xl w-72 h-96 gap-4 drop-shadow-lg">
-                        <div className={`w-full h-4 bg-primary rounded-t-2xl`} />
-                        <p className="text-xl text-black font-medium">Pro</p>
-                        <div className="flex flex-col items-center my-4 h-12">
-                            <p className="text-4xl text-black font-semibold">$4/mo</p>
-                            <p className="text-sm text-[#808080] mt-1">billed $48 annually</p>
-                        </div>
-                        <div className="flex flex-col items-center gap-2 w-full">
-                            <p>Up to 10 plans per month</p>
-                            <p>Multi Plan support</p>
-                        </div>
-                        <button
-                            className="flex items-center justify-center rounded-2xl mt-auto m-4 py-3 w-3/4 text-primary font-medium
-                        transition hover:scale-110 hover:bg-primary hover:text-white duration-300 border-2 border-primary"
-                            onClick={() => handleSubscribe(process.env.NEXT_PUBLIC_STRIPE_PRO_YEAR_PRICE_ID || "")}
-                            disabled={userData?.tier === 'Pro (year)'}
-                        >
-                            {(userData?.tier === 'Pro (year)') ? 'Your Plan'
-                                : (userData?.tier === 'Pro (month)') ? 'Switch' : 'Upgrade'
-                            }
-                        </button>
+                {/* Pro Plan */}
+                <div className="flex flex-col items-center bg-white rounded-2xl w-72 h-96 gap-4 drop-shadow-lg">
+                    <div className={`w-full h-4 bg-primary rounded-t-2xl`} />
+                    <p className="text-xl text-black font-medium">Pro</p>
+                    <div className="flex flex-col items-center my-4 h-12">
+                        <p className="text-4xl text-black font-semibold">$5/mo</p>
+                        <p className="text-sm text-[#808080] mt-1">billed monthly</p>
                     </div>
-                ) : (
-                    // Pro Plan (Monthly price)
-                    <div className="flex flex-col items-center bg-white rounded-2xl w-72 h-96 gap-4 drop-shadow-lg">
-                        <div className={`w-full h-4 bg-primary rounded-t-2xl`} />
-                        <p className="text-xl text-black font-medium">Pro</p>
-                        <div className="flex flex-col items-center my-4 h-12">
-                            <p className="text-4xl text-black font-semibold">$5/mo</p>
-                            <p className="text-sm text-[#808080] mt-1">billed monthly</p>
-                        </div>
-                        <div className="flex flex-col items-center gap-2 w-full">
-                            <p>Up to 10 plans per month</p>
-                            <p>Multi Plan support</p>
-                        </div>
-                        <button
-                            className="flex items-center justify-center rounded-2xl mt-auto m-4 py-3 w-3/4 text-primary font-medium
-                        transition hover:scale-110 hover:bg-primary hover:text-white duration-300 border-2 border-primary"
-                            onClick={() => handleSubscribe(process.env.NEXT_PUBLIC_STRIPE_PRO_MONTH_PRICE_ID || "")}
-                            disabled={userData?.tier === 'Pro (month)'}
-                        >
-                            {(userData?.tier === 'Pro (month)') ? 'Your Plan'
-                                : (userData?.tier === 'Pro (year)') ? 'Switch' : 'Upgrade'
-                            }
-                        </button>
+                    <div className="flex flex-col items-center gap-2 w-full">
+                        <p>Up to 10 plans per month</p>
+                        <p>Multi Plan support</p>
                     </div>
-                )}
+                    <button
+                        className="flex items-center justify-center rounded-2xl mt-auto m-4 py-3 w-3/4 text-primary font-medium
+                        transition hover:scale-110 hover:bg-primary hover:text-white duration-300 border-2 border-primary"
+                        onClick={() => handleSubscribe(process.env.NEXT_PUBLIC_STRIPE_PRO_MONTH_PRICE_ID || "")}
+                        disabled={userData?.tier === 'Pro (month)'}
+                    >
+                        {(userData?.tier === 'Pro (month)') ? 'Your Plan' : 'Upgrade'}
+                    </button>
+                </div>
             </div>
         </div >
     );
